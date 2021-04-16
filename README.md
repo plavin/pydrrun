4ed751e723221e2440e7613c8ac32c2487491cc9
Patrick Lavin <plavin997@gmail.com>
2021-04-16 09:53:56 -0400

    copied files from memtrace2npz

---
+++ README.md
@@ -0,0 +1,20 @@
+# pydrrun
+
+
+## Installation
+
+```
+pip3 install pydrrun
+```
+
+## Usage
+
+```
+todo
+```
+
+## Testing
+From this directory, run:
+```
+pytest
+```
---
+++ tests/test_conversion.py
@@ -0,0 +1,11 @@
+import pydrrun
+import pytest
+import tempfile
+import zipfile
+import os
+import filecmp
+import subprocess
+
+def test_conversion():
+
+    assert 1==1
